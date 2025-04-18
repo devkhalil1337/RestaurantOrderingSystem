a759bf2efdb8503c0f9085072b5b2949f1692d50
devkhalil1337 <devkhalil1337@gmail.com>
2025-04-18 22:22:40 +0500

    Initial Commit

---
+++ Restaurant.Core/Entites/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant.Core.Entites
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
